bd44b78ddbe57b5f23cceefbd4b7fead2e722d38
TIS-24 <shubham.kale@techinfini.com>
2021-12-23 14:20:59 +0530

    Trete

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //require("dotenv");
 //require('dotenv').config();
 
- const DB = "mongodb+srv://Tis216:Tis216@cluster0.ufju5.mongodb.net/Gcw?retryWrites=true&w=majority, { useNewUrlParser: true, useUnifiedTopology: true }";
+ const DB = "mongodb+srv://Tis216:Tis216@cluster0.ufju5.mongodb.net/Gcw?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 
 //const DB = process.env.DB;
 
